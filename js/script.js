53363d2c6b02e80136e7ce929eb1aaae8eeeb166
Andrey <sssmetannikov.a@gmail.com>
2023-04-04 16:53:40 +0700

    The_First_Practice

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 
 //Лекция  19
 
+const number0fFilms = +prompt('How many films did you watch?', '');
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilms = prompt('What is the last movie did you watch?', '');
+const score = prompt('How do you rate the film? 1-10', '');
+const lastFilms2 = prompt('What is the last movie did you watch?', '');
+const score2 = prompt('How do you rate the film? 1-10', '');
+
+
+personalMovieDB.movies[lastFilms] = score;
+personalMovieDB.movies[lastFilms2] = score2;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
